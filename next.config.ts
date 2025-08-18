eb88ef080d7e894678e9a8f6d3924628ff548050
Shakir-Bashir <shakir.786.external@gmail.com>
2025-08-18 22:34:46 +0530

    modify config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: ["lodash", "date-fns"], // example
+  },
 };
 
 export default nextConfig;
