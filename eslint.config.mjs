d4199f504a39ba216a017e39f5cc6a622bb19d3f
Shakir-Bashir <shakir.786.external@gmail.com>
2025-08-18 22:58:47 +0530

    modify eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,10 +12,12 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignorePatterns: ["lib/generated/**"],
+    ignores: ["lib/generated/**"],
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
