017662e4d23f94e4f69e0cd5b812232464a4cf63
Shakir-Bashir <shakir.786.external@gmail.com>
2025-08-19 09:46:51 +0530

    complete cart page subTotal-card shipping address zod schema shipping address form update user address checkout steps comp protecting paths

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -18,3 +18,11 @@ export const signUpdefaultValues = {
   password: "",
   confirmPassword: "",
 };
+
+export const shippingAddressDefaultValues = {
+  fullName: "",
+  streetAddress: "",
+  city: "",
+  postalCode: "",
+  country: "",
+};
--- lib/validators.ts
+++ lib/validators.ts
@@ -69,3 +69,14 @@ export const insertCartSchema = z.object({
   sessionCartId: z.string().min(1, "Session cart id is required"),
   userId: z.string().optional().nullable(),
 });
+
+//Schema for shipping address
+export const shippingAddressSchema = z.object({
+  fullName: z.string().min(3, "Name must be atleast 3 characters"),
+  streetAddress: z.string().min(3, "Address must be atleast 3 characters"),
+  city: z.string().min(3, "City must be atleast 3 characters"),
+  postalCode: z.string().min(3, "PostalCode must be atleast 3 characters"),
+  country: z.string().min(3, "Country must be atleast 3 characters"),
+  lat: z.number().optional(),
+  lng: z.number().optional(),
+});
