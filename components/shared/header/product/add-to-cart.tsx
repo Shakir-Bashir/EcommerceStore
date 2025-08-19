06dc44f389f8751569ac3c009872947daf9e0448
Shakir-Bashir <shakir.786.external@gmail.com>
2025-08-19 05:58:05 +0530

    add transition

--- components/shared/header/product/add-to-cart.tsx
+++ components/shared/header/product/add-to-cart.tsx
@@ -5,49 +5,44 @@ import { useRouter } from "next/navigation";
 import { Cart, CartItem } from "@/types";
 import { toast } from "sonner";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
-import { Plus, Minus } from "lucide-react";
+import { Plus, Minus, Loader } from "lucide-react";
+import { useTransition } from "react";
 
 const AddToCart = ({ item, cart }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
 
-    if (!res.success) {
-      toast.error(res.message);
-      return;
-    }
-
-    toast(res.message, {
-      action: {
-        label: "Go To Cart",
-        onClick: () => router.push("/cart"),
-      },
+      toast(res.message, {
+        action: {
+          label: "Go To Cart",
+          onClick: () => router.push("/cart"),
+        },
+      });
     });
   };
 
-  // const handleRemoveFromCart = async () => {
-  //   const res = await removeItemFromCart(item.productId);
-
-  //   toast({
-  //     variant: res.success ? "default" : "destructive",
-  //     description: res.message,
-  //   });
-  //   return;
-  // };
-
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    if (res.success) {
-      toast("Item removed from cart", {
-        description: res.message,
-      });
-    } else {
-      toast.error("Failed to remove item", {
-        description: res.message,
-      });
-    }
+      if (res.success) {
+        toast("Item removed from cart", {
+          description: res.message,
+        });
+      } else {
+        toast.error("Failed to remove item", {
+          description: res.message,
+        });
+      }
+    });
   };
 
   //Check if item is in cart
@@ -57,16 +52,28 @@ const AddToCart = ({ item, cart }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div>
       <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
-        <Minus className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Minus className="w-4 h-4" />
+        )}
       </Button>
       <span className="px-2">{existItem.qty}</span>
       <Button type="button" variant="outline" onClick={handleAddToCart}>
-        <Plus className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="w-4 h-4" />
+        )}
       </Button>
     </div>
   ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
-      <Plus /> Add To Cart
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <Plus className="w-4 h-4" />
+      )}
     </Button>
   );
 };
