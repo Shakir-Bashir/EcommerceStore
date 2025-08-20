0b43a054cc07f17f0c457269b761c85ae9f0f6ea
Shakir-Bashir <shakir.786.external@gmail.com>
2025-08-20 11:01:07 +0530

    complete payment-method-action&zod-schema payment-method-page-form order-ordrItem-prismaSchema order-page place-order-form utility-function orderDetailsTable

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import {
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
+  paymentMethodSchema,
 } from "../validators";
 import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
@@ -11,6 +12,7 @@ import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
 import { formatError } from "../utils";
 import { ShippingAddress } from "@/types";
+import { success, z } from "zod";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -108,6 +110,31 @@ export async function updateUserAddress(data: ShippingAddress) {
       success: true,
       message: "User updated successfully",
     };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Update users payment method
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+
+    if (!currentUser) throw new Error("User not found");
+
+    const paymentMethod = paymentMethodSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+
+    return { success: true, message: "User updated successfully" };
   } catch (error) {
     return { success: false, message: formatError(error) };
   }
--- lib/validators.ts
+++ lib/validators.ts
@@ -1,6 +1,7 @@
 // import { format } from "path";
 import { z } from "zod";
 import { formateNumberWithDecimal } from "./utils";
+import { PAYMENT_METHODS } from "./constants";
 
 // const currency = z
 //   .string()
@@ -79,4 +80,37 @@ export const shippingAddressSchema = z.object({
   country: z.string().min(3, "Country must be atleast 3 characters"),
   lat: z.number().optional(),
   lng: z.number().optional(),
+});
+
+// Schema for Payment method
+export const paymentMethodSchema = z
+  .object({
+    type: z.string().min(1, "Payment method is required"),
+  })
+  .refine((data) => PAYMENT_METHODS.includes(data.type), {
+    path: ["type"],
+    message: "Invalid payment method",
+  });
+
+// Schema for inserting order
+export const insertOrderSchema = z.object({
+  userId: z.string().min(1, "user is required"),
+  itemsPrice: currency,
+  shippingPrice: currency,
+  totalPrice: currency,
+  taxPrice: currency,
+  paymentMethod: z.string().refine((data) => PAYMENT_METHODS.includes(data), {
+    message: "Invalid payment method",
+  }),
+  shippingAddress: shippingAddressSchema,
+});
+
+// Schema for inserting an order item
+export const insertOrderItemSchema = z.object({
+  productId: z.string(),
+  slug: z.string(),
+  image: z.string(),
+  name: z.string(),
+  price: currency,
+  qty: z.number(),
 });
