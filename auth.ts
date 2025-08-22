05d7b3bf182a512018b5c2a34a0b0ef1e350405d
Shakir-Bashir <shakir.786.external@gmail.com>
2025-08-22 18:28:12 +0530

    complete userlayout menue orders action page & pagination update profile action form

--- auth.ts
+++ auth.ts
@@ -108,6 +108,10 @@ export const config = {
           }
         }
       }
+      // Handle session updates
+      if (session?.user.name && trigger === "update") {
+        token.name = session.user.name;
+      }
 
       return token;
     },
